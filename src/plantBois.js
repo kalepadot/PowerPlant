312782db59d4ff991f7c83ed6bd9edda38ae9196
Jeremy Kale Padot <jeremy@padot.us>
2020-04-14 15:56:50 -0700

    add a second plant with all working buttons
    Co-authored-by: Patrick Delaney <prestwick97@gmail.com>
    Co-authored-by: Benjamin Thom <jaminthom@gmail.com>

--- src/plantBois.js
+++ src/plantBois.js
@@ -34,6 +34,7 @@ const storeState = () => {
 }
 
 const stateChanger = storeState();
+// const feedPlant1 = stateChanger();
 // const fedPlant = stateChanger(blueFood);
 // const plantFedAgain = stateChanger(greenFood);
 // const unFedPlant = stateChanger(yuckyFood);
@@ -41,8 +42,10 @@ const stateChanger = storeState();
 // console.log(plantFedAgain);
 // console.log(unFedPlant);
 //new plant
-// const newPlant = storeState();
-// const fedzPlant = newPlant(blueFood);
+const newPlant = storeState();
+
+// const feedPlant2 = newPlant();
+// const feedNewPlant = newPlant(blueFood);
 
 // this is from plant.js whats communicating with the ui
 // const feed = changeState("soil");
@@ -64,5 +67,72 @@ $(document).ready(function() {
   $('#yuckyFood').click(function() {
     const newState = stateChanger(yuckyFood);
     $('#soil-value').text(newState.soil);
+  });
+  
+  $('#blueWater').click(function() {
+    const newState = stateChanger(blueWater);
+    $('#water-value').text(newState.water);
+  });
+  $('#greenWater').click(function() {
+    const newState = stateChanger(greenWater);
+    $('#water-value').text(newState.water);
+  });
+  $('#yuckyWater').click(function() {
+    const newState = stateChanger(yuckyWater);
+    $('#water-value').text(newState.water);
+  });
+
+  $('#blueLight').click(function() {
+    const newState = stateChanger(blueLight);
+    $('#light-value').text(newState.light);
+  });
+  $('#greenLight').click(function() {
+    const newState = stateChanger(greenLight);
+    $('#light-value').text(newState.light);
+  });
+  $('#yuckyLight').click(function() {
+    const newState = stateChanger(yuckyLight);
+    $('#light-value').text(newState.light);
+  });
+
+  //new plant--------------------------------------------
+
+  $('#blueFood2').click(function() {
+    const newState = newPlant(blueFood);
+    $('#soil-value2').text(newState.soil);
+  });
+  $('#greenFood2').click(function() {
+    const newState = newPlant(greenFood);
+    $('#soil-value2').text(newState.soil);
+  });
+  $('#yuckyFood2').click(function() {
+    const newState = newPlant(yuckyFood);
+    $('#soil-value2').text(newState.soil);
+  });
+
+  $('#blueWater2').click(function() {
+    const newState = newPlant(blueWater);
+    $('#water-value2').text(newState.water);
+  });
+  $('#greenWater2').click(function() {
+    const newState = newPlant(greenWater);
+    $('#water-value2').text(newState.water);
+  });
+  $('#yuckyWater2').click(function() {
+    const newState = newPlant(yuckyWater);
+    $('#water-value2').text(newState.water);
+  });
+
+  $('#blueLight2').click(function() {
+    const newState = stateChanger(blueLight);
+    $('#light-value2').text(newState.light);
+  });
+  $('#greenLight2').click(function() {
+    const newState = stateChanger(greenLight);
+    $('#light-value2').text(newState.light);
+  });
+  $('#yuckyLight2').click(function() {
+    const newState = stateChanger(yuckyLight);
+    $('#light-value2').text(newState.light);
   });
 });
